7d8c1a7490585fb3170326a4fdc85e13179ec108
na-ttt-an <z.alekto@gmail.com>
2024-01-25 22:37:43 +0300

    init project

---
+++ test.py
@@ -0,0 +1,7 @@
+a = float(input('введите переменную a: '))
+b = float(input('введите переменную b: '))
+c = float(input('введите переменную c: '))
+y = 0
+for x in range(10000):
+    (a*x**2 + b*x + c) == 0
+print(x)
